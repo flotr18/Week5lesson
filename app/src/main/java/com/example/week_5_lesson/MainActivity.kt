2540b6d7c2e778e79a6a9e71ca052dccc4a0f44c
flotr18 <55090943+flotr18@users.noreply.github.com>
2020-10-15 15:42:23 +0100

    Added Hello World

--- app/src/main/java/com/example/week_5_lesson/MainActivity.kt
+++ app/src/main/java/com/example/week_5_lesson/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.week_5_lesson
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+// This is feature 1
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
